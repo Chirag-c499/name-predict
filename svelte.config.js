86219cd3ca68198bbba473bc8fff70894c0e9e9d
Chirag-c499 <cg338655@gmail.com>
2025-06-27 18:58:44 +0530

    svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
