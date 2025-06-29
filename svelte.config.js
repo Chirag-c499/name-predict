74d4cd7c5b7d5faeba77af5619fb4d3588c54317
Chirag-c499 <cg338655@gmail.com>
2025-06-29 10:30:05 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  kit: {
-    adapter: vercel(),
-    // Optional: trailingSlash, prerender, etc.
-  },
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter()
+  }
 };
+
+export default config;
